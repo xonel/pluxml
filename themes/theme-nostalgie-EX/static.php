8cf49bab3ed45272e9f19d8923db5a458a1482c2
xonel lenox <le.nox@free.fr>
2011-12-23 02:41:36 +0100

    rm

--- themes/theme-nostalgie-EX/static.php
+++
@@ -1,9 +0,0 @@
-<?php include(dirname(__FILE__).'/header.php'); # On insere le header ?>
-<div id="page">
-	<div id="content">
-		<h2 class="title static"><?php $plxShow->staticTitle(); ?></h2>
-		<div class="post"><?php $plxShow->staticContent(); ?></div>
-	</div>
-	<?php include(dirname(__FILE__).'/sidebar.php'); # On insere la sidebar ?>
-</div>
-<?php include(dirname(__FILE__).'/footer.php'); # On insere le footer ?>
